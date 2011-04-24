90ab15cb430ba5b5d42c93d64cefb51455b39869
Tarick Bedeir <tarick@bedeir.com>
2011-04-24 02:13:12 +0000

    32-bit compile fixes

--- src/stats_cache.hh
+++ src/stats_cache.hh
@@ -30,7 +30,7 @@ namespace s3
 
       S3_DEBUG(
         "stats_cache::~stats_cache", 
-        "hits: %li (%.02f%%), misses: %li (%.02f%%), expiries: %li (%.02f%%)\n", 
+        "hits: %lli (%.02f%%), misses: %lli (%.02f%%), expiries: %lli (%.02f%%)\n", 
         _hits,
         double(_hits) / double(total) * 100.0,
         _misses,
