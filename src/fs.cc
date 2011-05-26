9e72806a87dbb52cb54d115f316bf3db97abfd71
Tarick Bedeir <tarick@bedeir.com>
2011-05-26 16:29:09 +0000

    add todo for multiple buckets

--- src/fs.cc
+++ src/fs.cc
@@ -19,6 +19,7 @@ using namespace std;
 using namespace s3;
 
 // TODO: try/catch everywhere! (or, get rid of exceptions)
+// TODO: support multiple buckets (with a different set of credentials each)
 
 #define ASSERT_NO_TRAILING_SLASH(str) do { if ((str)[(str).size() - 1] == '/') return -EINVAL; } while (0)
 
