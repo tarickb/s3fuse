1cd7115fbd665826a22f5c1b3d35e8bc5146d528
Tarick Bedeir <tarick@bedeir.com>
2012-07-10 04:05:02 +0000

    add compute_md5 for string input

--- src/util.h
+++ src/util.h
@@ -33,7 +33,8 @@ namespace s3
   enum md5_output_type
   {
     MOT_BASE64,
-    MOT_HEX
+    MOT_HEX,
+    MOT_HEX_NO_QUOTE
   };
 
   class util
@@ -41,11 +42,17 @@ namespace s3
   public:
     static std::string base64_encode(const uint8_t *input, size_t size);
     static void base64_decode(const std::string &input, std::vector<uint8_t> *output);
+
     static std::string sign(const std::string &key, const std::string &data);
+
     static std::string compute_md5(int fd, md5_output_type type = MOT_BASE64, ssize_t size = 0, off_t offset = 0);
+    static std::string compute_md5(const std::string &input, md5_output_type type = MOT_BASE64);
+
     static std::string url_encode(const std::string &url);
     static std::string hex_encode(const uint8_t *input, size_t size);
+
     static double get_current_time();
+
     static bool is_valid_md5(const std::string &md5);
     static bool is_valid_http_string(const std::string &value);
   };
