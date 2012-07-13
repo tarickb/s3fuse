5d29650c075a263a354f33df07789ecf82a85de7
Tarick Bedeir <tarick@bedeir.com>
2012-07-13 18:09:39 +0000

    unify build scripts

--- build-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VERSION=`head -n 1 ChangeLog | sed -e 's/.*(//' -e 's/).*//'`
-
-cat configure.ac.in | sed -e "s/__VERSION__/$VERSION/g" > configure.ac
-autoreconf --force --install || exit 1
-./configure --enable-darwin
--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ VERSION=`head -n 1 ChangeLog | sed -e 's/.*(//' -e 's/).*//'`
 
 cat configure.ac.in | sed -e "s/__VERSION__/$VERSION/g" > configure.ac
 autoreconf --force --install || exit 1
-./configure
+
+if [ "$(uname)" == "Darwin" ]; then
+  ./configure --enable-darwin
+else
+  ./configure
+fi
