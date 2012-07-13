3dd5764363921f49ea253e7e08f04631258ace33
Tarick Bedeir <tarick@bedeir.com>
2012-07-13 18:10:46 +0000

    make xattr prefix a config setting

--- src/object.cc
+++ src/object.cc
@@ -39,13 +39,9 @@ namespace
   const int     BLOCK_SIZE                    = 512;
   const string  META_PREFIX                   = "meta-";
   const string  META_PREFIX_RESERVED          = "s3fuse-";
-  const string  ATTR_USER_PREFIX              = "user.";
 
   const char   *META_PREFIX_RESERVED_CSTR     = META_PREFIX_RESERVED.c_str();
   const size_t  META_PREFIX_RESERVED_LEN      = META_PREFIX_RESERVED.size();
-
-  const char   *ATTR_USER_PREFIX_CSTR         = ATTR_USER_PREFIX.c_str();
-  const size_t  ATTR_USER_PREFIX_LEN          = ATTR_USER_PREFIX.size();
 
   const char   *SYMLINK_CONTENT_TYPE          = "text/symlink";
 
@@ -108,10 +104,10 @@ void object::init_stat()
 int object::set_metadata(const string &key, const char *value, size_t size, int flags)
 {
   mutex::scoped_lock lock(_metadata_mutex);
-  string user_key = key.substr(ATTR_USER_PREFIX_LEN);
+  string user_key = key.substr(config::get_xattr_prefix().size());
   xattr_map::iterator itor = _metadata.find(user_key);
 
-  if (strncmp(key.c_str(), ATTR_USER_PREFIX_CSTR, ATTR_USER_PREFIX_LEN) != 0)
+  if (key.substr(0, config::get_xattr_prefix().size()) != config::get_xattr_prefix())
     return -EINVAL;
 
   if (strncmp(user_key.c_str(), META_PREFIX_RESERVED_CSTR, META_PREFIX_RESERVED_LEN) == 0)
@@ -139,21 +135,21 @@ void object::get_metadata_keys(vector<string> *keys)
 {
   mutex::scoped_lock lock(_metadata_mutex);
 
-  keys->push_back(ATTR_USER_PREFIX + "__md5__");
-  keys->push_back(ATTR_USER_PREFIX + "__etag__");
-  keys->push_back(ATTR_USER_PREFIX + "__content_type__");
+  keys->push_back(config::get_xattr_prefix() + "__md5__");
+  keys->push_back(config::get_xattr_prefix() + "__etag__");
+  keys->push_back(config::get_xattr_prefix() + "__content_type__");
 
   for (xattr_map::const_iterator itor = _metadata.begin(); itor != _metadata.end(); ++itor)
-    keys->push_back(ATTR_USER_PREFIX + itor->first);
+    keys->push_back(config::get_xattr_prefix() + itor->first);
 }
 
 int object::get_metadata(const string &key, char *buffer, size_t max_size)
 {
   mutex::scoped_lock lock(_metadata_mutex);
   xattr::ptr value;
-  string user_key = key.substr(ATTR_USER_PREFIX_LEN);
+  string user_key = key.substr(config::get_xattr_prefix().size());
 
-  if (strncmp(key.c_str(), ATTR_USER_PREFIX_CSTR, ATTR_USER_PREFIX_LEN) != 0)
+  if (key.substr(0, config::get_xattr_prefix().size()) != config::get_xattr_prefix())
     return -EINVAL;
 
   if (user_key == "__md5__")
@@ -175,7 +171,7 @@ int object::get_metadata(const string &key, char *buffer, size_t max_size)
 int object::remove_metadata(const string &key)
 {
   mutex::scoped_lock lock(_metadata_mutex);
-  xattr_map::iterator itor = _metadata.find(key.substr(ATTR_USER_PREFIX_LEN));
+  xattr_map::iterator itor = _metadata.find(key.substr(config::get_xattr_prefix().size()));
 
   if (itor == _metadata.end())
     return -ENODATA;
