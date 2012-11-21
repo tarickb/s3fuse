47806f1959c7d4e8de8c654d7a62e7b8a2247dd3
Tarick Bedeir <tarick@bedeir.com>
2012-11-21 06:39:01 +0000

    add tag to worker create method

--- src/threads/worker.h
+++ src/threads/worker.h
@@ -15,7 +15,7 @@ namespace s3
     public:
       typedef boost::shared_ptr<worker> ptr;
 
-      static ptr create(const boost::shared_ptr<work_item_queue> &queue)
+      static ptr create(const boost::shared_ptr<work_item_queue> &queue, const std::string & /* ignored */)
       {
         ptr wt(new worker(queue));
 
