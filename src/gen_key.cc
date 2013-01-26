0845ddd68708f7359e2a34a721005b5ca715ebb4
Tarick Bedeir <tarick@bedeir.com>
2013-01-26 16:36:00 +0000

    more gen-key changes

--- src/gen_key.cc
+++ src/gen_key.cc
@@ -1,5 +1,6 @@
 #include <getopt.h>
 
+#include <fstream>
 #include <iostream>
 
 #include "base/config.h"
@@ -7,6 +8,7 @@
 #include "base/xml.h"
 #include "crypto/buffer.h"
 #include "crypto/passwords.h"
+#include "crypto/private_file.h"
 #include "fs/bucket_volume_key.h"
 #include "fs/encryption.h"
 #include "services/service.h"
@@ -16,6 +18,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::getline;
+using std::ifstream;
+using std::ofstream;
 using std::runtime_error;
 using std::string;
 
@@ -24,21 +28,31 @@ using s3::base::logger;
 using s3::base::xml;
 using s3::crypto::buffer;
 using s3::crypto::passwords;
+using s3::crypto::private_file;
 using s3::fs::bucket_volume_key;
 using s3::fs::encryption;
 using s3::services::service;
 
 namespace
 {
-  // TODO: parameter to force larger key?
-  // TODO: parameters for files
-
-  const char *SHORT_OPTIONS = ":c:f";
+  const char *SHORT_OPTIONS = ":c:i:o:";
 
   const option LONG_OPTIONS[] = {
     { "config-file", required_argument, NULL, 'c'  },
-    { "delete-key",  no_argument,       NULL, 'r'  },
+    { "in-key",      required_argument, NULL, 'i'  },
+    { "out-key",     required_argument, NULL, 'o'  },
     { NULL,          NULL,              NULL, NULL }};
+}
+
+void init(const string &config_file)
+{
+  logger::init(LOG_ERR);
+  config::init(config_file);
+  service::init(config::get_service());
+  xml::init(service::get_xml_namespace());
+
+  if (!config::get_use_encryption())
+    throw runtime_error("encryption not enabled in config file");
 }
 
 string to_lower(string in)
@@ -56,14 +70,6 @@ void confirm_key_delete()
   string line;
 
   cout <<
-    "*******************************************************************\n"
-    "* WARNING                                                         *\n"
-    "* -------                                                         *\n"
-    "*                                                                 *\n"
-    "* What you are about to do will render inaccessible all encrypted *\n"
-    "* objects in this bucket.  This operation is irreversible.        *\n"
-    "*******************************************************************\n"
-    "\n"
     "You are going to delete the volume encryption key for bucket:\n"
     "  " << config::get_bucket_name() << "\n"
     "\n"
@@ -74,7 +80,17 @@ void confirm_key_delete()
   if (line != config::get_bucket_name())
     throw runtime_error("aborted");
 
-  cout << "Do you understand that existing encrypted files will be lost forever? Type \"yes\": ";
+  cout <<
+    "\n"
+    "*******************************************************************\n"
+    "* WARNING                                                         *\n"
+    "* -------                                                         *\n"
+    "*                                                                 *\n"
+    "* What you are about to do will render inaccessible all encrypted *\n"
+    "* objects in this bucket.  This operation is irreversible.        *\n"
+    "*******************************************************************\n"
+    "\n"
+    "Do you understand that existing encrypted files will be lost forever? Type \"yes\": ";
 
   getline(cin, line);
   line = to_lower(line);
@@ -91,7 +107,7 @@ void confirm_key_delete()
     throw runtime_error("aborted");
 }
 
-buffer::ptr get_current_password_key()
+buffer::ptr prompt_for_current_password()
 {
   string current_password;
 
@@ -103,7 +119,7 @@ buffer::ptr get_current_password_key()
   return encryption::derive_key_from_password(current_password);
 }
 
-buffer::ptr get_new_password_key()
+buffer::ptr prompt_for_new_password()
 {
   string new_password, confirm;
 
@@ -120,22 +136,158 @@ buffer::ptr get_new_password_key()
   return encryption::derive_key_from_password(new_password);
 }
 
+buffer::ptr read_key_from_file(const string &file)
+{
+  ifstream f;
+  string line;
+
+  cout << "Reading key from [" << file << "]..." << endl;
+  private_file::open(file, &f);
+  getline(f, line);
+
+  return buffer::from_string(line);
+}
+
+buffer::ptr generate_and_write(const string &file)
+{
+  ofstream f;
+  buffer::ptr key;
+
+  key = buffer::generate(bucket_volume_key::key_cipher::DEFAULT_KEY_LEN);
+
+  cout << "Writing key to [" << file << "]..." << endl;
+  private_file::open(file, &f);
+
+  f << key->to_string() << endl;
+
+  return key;
+}
+
+void generate_new_key(const string &config_file, const string &out_key_file)
+{
+  buffer::ptr password_key;
+
+  init(config_file);
+
+  if (bucket_volume_key::is_present())
+    throw runtime_error("bucket already contains a volume key");
+
+  cout <<
+    "This bucket does not currently have an encryption key. We'll create one.\n"
+    "\n";
+
+  if (out_key_file.empty())
+    password_key = prompt_for_new_password();
+  else
+    password_key = generate_and_write(out_key_file);
+
+  cout << "Generating volume key..." << endl;
+  bucket_volume_key::generate(password_key);
+
+  cout << "Done." << endl;
+}
+
+void reencrypt_key(const string &config_file, const string &in_key_file, const string &out_key_file)
+{
+  buffer::ptr current_key, new_key;
+
+  init(config_file);
+
+  if (!bucket_volume_key::is_present())
+    throw runtime_error("bucket does not contain a volume key");
+
+  cout << 
+    "Bucket already contains an encryption key.\n"
+    "\n"
+    "If you've forgotten the password for this bucket, or lost the local password\n"
+    "key, use the \"delete\" option to delete the volume key (and permanently lose\n"
+    "any files that are currently encrypted).\n"
+    "\n";
+
+  if (in_key_file.empty())
+    current_key = prompt_for_current_password();
+  else
+    current_key = read_key_from_file(in_key_file);
+
+  if (out_key_file.empty())
+    new_key = prompt_for_new_password();
+  else
+    new_key = generate_and_write(out_key_file);
+
+  cout << "Changing key..." << endl;
+  bucket_volume_key::reencrypt(current_key, new_key);
+
+  cout << "Done." << endl;
+}
+
+void delete_key(const string &config_file)
+{
+  init(config_file);
+
+  if (!bucket_volume_key::is_present())
+    throw runtime_error("bucket does not contain a volume key");
+
+  confirm_key_delete();
+
+  cout << "Deleting key..." << endl;
+  bucket_volume_key::remove();
+
+  cout << "Done." << endl;
+}
+
 void print_usage(const char *arg0)
 {
   const char *base_name = strrchr(arg0, '/');
 
   base_name = base_name ? base_name + 1 : arg0;
 
-  cerr << "Usage: " << base_name << " [--config-file <path>] [--delete-key]" << endl;
+  cerr << 
+    "Usage: " << base_name << " <command> [options]\n"
+    "\n"
+    "Where <command> is one of:\n"
+    "\n"
+    "  generate                  Generate a new volume key and write it to the bucket.\n"
+    "  change                    Change the password or key file used to access the\n"
+    "                            volume key stored in the bucket.\n"
+    "  delete                    Erase the bucket volume key.\n"
+    "\n"
+    "[options] can be:\n"
+    "\n"
+    "  -c, --config-file <path>  Use configuration at <path> rather than the default.\n"
+    "  -i, --in-key <path>       Use key at <path> rather than prompting for the current\n"
+    "                            volume password (only valid with \"change\").\n"
+    "  -o, --out-key <path>      Store key at <path> rather than prompting for a new\n"
+    "                            volume password.  The operation will fail if a file\n"
+    "                            exists at this path (only valid with \"generate\" and\n"
+    "                            \"change\").\n"
+    "\n"
+    "Examples:\n"
+    "\n"
+    "  Switching from prompting for a password when mounting a volume to using a saved\n"
+    "  key file:\n"
+    "\n"
+    "    " << base_name << " change --out-key path-to-new-key-file\n"
+    "\n"
+    "  Switching from saved keys to password prompts:\n"
+    "\n"
+    "    " << base_name << " change --in-key path-to-current-key-file\n"
+    "\n"
+    "  Generating a new volume key, with password prompting:\n"
+    "\n"
+    "    " << base_name << " generate\n"
+    "\n"
+    "  Generating a new volume key, with saved key file:\n"
+    "\n"
+    "    " << base_name << " generate --out-key path-to-new-key-file" << endl;
 
   exit(1);
 }
 
 int main(int argc, char **argv)
 {
-  int opt;
-  string config_file;
-  bool delete_key = false;
+  int opt = 0;
+  string config_file, in_key_file, out_key_file;
+  string command;
 
   while ((opt = getopt_long(argc, argv, SHORT_OPTIONS, LONG_OPTIONS, NULL)) != -1) {
     switch (opt) {
@@ -143,8 +295,12 @@ int main(int argc, char **argv)
         config_file = optarg;
         break;
 
-      case 'r':
-        delete_key = true;
+      case 'i':
+        in_key_file = optarg;
+        break;
+
+      case 'o':
+        out_key_file = optarg;
         break;
 
       default:
@@ -152,63 +308,30 @@ int main(int argc, char **argv)
     }
   }
 
+  if (optind < argc)
+    command = argv[optind];
+
   try {
-    logger::init(LOG_ERR);
-    config::init(config_file);
-    service::init(config::get_service());
-    xml::init(service::get_xml_namespace());
+    if (command == "generate") {
+      if (!in_key_file.empty())
+        print_usage(argv[0]);
 
-    if (!config::get_use_encryption())
-      throw runtime_error("encryption not enabled in config file");
+      generate_new_key(config_file, out_key_file);
 
-    if (delete_key) {
-      if (!bucket_volume_key::is_present())
-        throw runtime_error("bucket does not contain a volume key");
+    } else if (command == "change") {
+      reencrypt_key(config_file, in_key_file, out_key_file);
 
-      confirm_key_delete();
+    } else if (command == "delete") {
+      if (!in_key_file.empty() || !out_key_file.empty())
+        print_usage(argv[0]);
 
-      cout << "Deleting key..." << endl;
-      bucket_volume_key::remove();
-
-      cout << "Done." << endl;
-      return 0;
-    }
-
-    if (bucket_volume_key::is_present()) {
-      buffer::ptr current_password_key, new_password_key;
-
-      cout << 
-        "Bucket already contains an encryption key.\n"
-        "\n"
-        "If you've forgotten the password for this bucket, or lost the local password\n"
-        "key, pass \"--delete-key\" to delete the volume key (and permanently lose any\n"
-        "files that are currently encrypted).\n"
-        "\n";
-
-      current_password_key = get_current_password_key();
-      new_password_key = get_new_password_key();
-
-      cout << "Changing key..." << endl;
-      bucket_volume_key::reencrypt(current_password_key, new_password_key);
-
-      cout << "Done." << endl;
-      return 0;
+      delete_key(config_file);
 
     } else {
-      buffer::ptr new_password_key;
-
-      cout <<
-        "This bucket does not currently have an encryption key. We'll create one.\n"
-        "\n";
-
-      new_password_key = get_new_password_key();
-
-      cout << "Generating volume key..." << endl;
-      bucket_volume_key::generate(new_password_key);
-
-      cout << "Done." << endl;
-      return 0;
+      print_usage(argv[0]);
     }
+
+    return 0;
 
   } catch (const std::exception &e) {
     cout << "Caught exception: " << e.what() << endl;
