4fb884733b602848ae885770b14028ea88d3033d
Tarick Bedeir <tarick@bedeir.com>
2013-01-27 19:30:34 +0000

    add signal handler to restore echo after catching SIGINT or SIGTERM

--- src/crypto/passwords.cc
+++ src/crypto/passwords.cc
@@ -19,6 +19,8 @@
  * limitations under the License.
  */
 
+#include <signal.h>
+#include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
@@ -35,17 +37,40 @@ using std::string;
 
 using s3::crypto::passwords;
 
+namespace
+{
+  termios s_term_flags_orig;
+
+  void reset_term_attrs(int, siginfo_t *, void *)
+  {
+    tcsetattr(STDIN_FILENO, TCSANOW, &s_term_flags_orig);
+    exit(1);
+  }
+}
+
 string passwords::read_from_stdin(const string &prompt)
 {
   string line;
-  termios term_flags, term_flags_orig;
+  termios term_flags;
+  struct sigaction new_action, old_int_action, old_term_action;
 
-  tcgetattr(STDIN_FILENO, &term_flags_orig);
-  memcpy(&term_flags, &term_flags_orig, sizeof(term_flags));
+  memset(&new_action, 0, sizeof(new_action));
+  memset(&old_int_action, 0, sizeof(old_int_action));
+  memset(&old_term_action, 0, sizeof(old_term_action));
+
+  new_action.sa_sigaction = reset_term_attrs;
+  new_action.sa_flags = SA_SIGINFO;
+
+  tcgetattr(STDIN_FILENO, &s_term_flags_orig);
+  memcpy(&term_flags, &s_term_flags_orig, sizeof(term_flags));
 
   cout << prompt;
 
   term_flags.c_lflag &= ~ECHO;
+
+  sigaction(SIGINT, &new_action, &old_int_action);
+  sigaction(SIGTERM, &new_action, &old_term_action);
+
   tcsetattr(STDIN_FILENO, TCSANOW, &term_flags);
 
   try {
@@ -54,7 +79,10 @@ string passwords::read_from_stdin(const string &prompt)
     line.clear();
   }
 
-  tcsetattr(STDIN_FILENO, TCSANOW, &term_flags_orig);
+  tcsetattr(STDIN_FILENO, TCSANOW, &s_term_flags_orig);
+
+  sigaction(SIGINT, &old_int_action, NULL);
+  sigaction(SIGTERM, &old_term_action, NULL);
 
   cout << endl;
 
