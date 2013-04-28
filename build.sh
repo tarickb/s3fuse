a1038efbd1999515fc143d85d60d5c19c7e16492
Tarick Bedeir <tarick@bedeir.com>
2013-04-28 21:06:11 +0000

    build with revision by default

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ "$1" == "--with-rev" ]; then
+WITH_REV=1
+
+if [ "$1" == "--no-rev" ]; then
   shift
-  export WITH_REV=1
+  WITH_REV=0
 fi
+
+export WITH_REV
 
 autoreconf --force --install || exit 1
 
